7f6b7fd53640640c01cb391e2079acedd2dff309
KIPSITO <61295220+DOPEKILL431@users.noreply.github.com>
2021-02-23 20:25:17 +0000

    Add files via upload
    PRGM pour dé

---
+++ Dé.py
@@ -0,0 +1,14 @@
+#PROGRAMME POUR CHOISIR UN NOMBRE
+from random import*
+
+résultat_aléatoire=0
+
+a=int(input("saisir le nombre minimum : "))
+b=int(input("saisir le nombre maximum : "))
+
+résultat_aléatoire=randint(a,b)
+
+print(résultat_aléatoire)
+
+
+
